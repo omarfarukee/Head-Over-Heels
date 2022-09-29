b1e35cf07eb81e1840424568aae6bda670ec3e9f
omarfarukee <umarfaruque1920@gmail.com>
2022-09-29 10:22:45 +0600

    cart done

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Tools from '../Tools/Tools';
+import img from '../../images/IMG_0192 (2).JPG'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import ('./Body.css')
 const Body = () => {
     const [element, setElement] = useState([]);
 
     useEffect( () =>{
-        fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+        fetch('products.json')
         .then(res => res.json())
-        .then(data => { setElement(data.meals)})
+        .then(data => setElement(data))
     }, [])
     // useEffect(()=>{
     //     fetch('products.json')
@@ -30,7 +33,44 @@ const Body = () => {
 
             </div>
             <div className='activity-container'>
-                <h2>my activity</h2>
+                <div className='cart'>
+                    <img src={img} alt="" />
+                    <div className='cart-details'>
+                        <h3>Omar Faruk</h3>
+                        <p><FontAwesomeIcon icon={faLocationDot}/> Dhaka, Bangladesh</p>
+                    </div>
+                </div>
+                <div className='about-me'>
+                    <div>
+                        <h2>65<small className='small'>kg</small></h2>
+                        <p className='under'>Weight</p>
+                    </div>
+                    <div>
+                        <h2>5.8<small className='small'>ft.</small></h2>
+                        <p className='under'>Height</p>
+                    </div>
+                    <div>
+                        <h2>21<small className='small'>yr.</small></h2>
+                        <p className='under'>age</p>
+                    </div>
+                </div>
+                <h2 className='break'>Break-time-add</h2>
+                <div className='time-btn'>
+                    <button>45s</button>
+                    <button>60s</button>
+                    <button>75s</button>
+                    <button>99s</button>
+                </div>
+                <h3 className='break'>Exercise-information</h3>
+                <div className='Exercise-Time'>
+                    <h4>Exercise-Time</h4>
+                    <p>00s</p>
+                </div>
+                <div className='Exercise-Time'>
+                    <h4>Break-time</h4>
+                    <p>00s</p>
+                </div>
+                <button className='see-btn'>See activity</button>
             </div>
         </div>
     );
--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faFolderPlus } from '@fortawesome/free-solid-svg-icons'
 import ('./Tools.css')
 const Tools = (props) => {
     console.log(props)
-    const {strMeal,strMealThumb} = props.elements
+    const {name, img} = props.elements
     return (
         <div className='tools-container'>
             <div>
-                <img src={strMealThumb} alt="" />
-                <h3>{strMeal}</h3>
+                <img src={img} alt="" />
+                <h3>{name}</h3>
                 <h2>time : 30s</h2>  
-                <button className='btn-add'>Add to list</button>
+                <button className='btn-add'>Add to list <FontAwesomeIcon icon={faFolderPlus} /> </button>
         </div>
 
         </div>
