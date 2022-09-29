71b0da8398e458033d58f8eb9fc688f47210ff0f
omarfarukee <umarfaruque1920@gmail.com>
2022-09-29 11:23:02 +0600

    body All done

--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -4,13 +4,14 @@ import { faCoffee, faFolderPlus } from '@fortawesome/free-solid-svg-icons'
 import ('./Tools.css')
 const Tools = (props) => {
     console.log(props)
-    const {name, img} = props.elements
+    const {name, img, details, time} = props.elements
     return (
         <div className='tools-container'>
             <div>
                 <img src={img} alt="" />
                 <h3>{name}</h3>
-                <h2>time : 30s</h2>  
+                <p>{details}</p>
+                <h2>time :{time}s</h2>  
                 <button className='btn-add'>Add to list <FontAwesomeIcon icon={faFolderPlus} /> </button>
         </div>
 
